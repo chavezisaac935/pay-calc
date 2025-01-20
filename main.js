0f71865074a338b6b7342ee1a5f9874196fca345
Isaac Chavez <chavez.isaac935@gmail.com>
2025-01-20 11:58:15 -0800

    Retreived input from user and stored to variables

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-let hoursWorked = document.getElementById("hoursWorked").value;
-console.log(hoursWorked);
+// console.log(hoursWorked);
+let hoursWorkedInput;
+let payRateInput;
+let stateAbbreviationInput;
+
+const button = document.getElementById("calculateButton");
+button.addEventListener("click", (event) => {
+    hoursWorkedInput = document.getElementById("hoursWorked").value;
+    console.log(hoursWorkedInput);
+    
+    payRateInput = document.getElementById("payRate").value;
+    console.log(payRateInput);
+    
+    stateAbreviationInput = document.getElementById("stateAbbreviation").value;
+    console.log(stateAbreviationInput);
+});
